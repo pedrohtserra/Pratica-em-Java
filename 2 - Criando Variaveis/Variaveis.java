9c4da4d4b010ed500d0b5dc8740d5ff733292fdb
pedrohtserra <pepeserra2023@gmail.com>
2026-01-28 20:53:42 -0300

    Primeiro Commit

---
+++ 2 - Criando Variaveis/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis {
+    public static void main(String[] args){
+        String nome = "Pedro";
+        int a = 5;
+        float b = 5.5f;
+        char c = 'C';
+        boolean booleano = true;
+        
+        System.out.println(nome);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(booleano);
+    }
+}
---
+++ 3 - Constants/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static void main (String[] args){
+        final int minutos_por_hora = 60;
+
+        System.out.println("Duas horas tem " + minutos_por_hora * 2 + " minutos.");
+    }
+}
