721b95bd87d4b402704f1961fae982f17f062eb6
pedrohtserra <pepeserra2023@gmail.com>
2026-01-30 20:54:37 -0300

    ArrayList adicionado

---
+++ 2.1 - ArrayList/praticaArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class praticaArrayList {
+    public static void main (String[] args){
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Pedro");
+        nomes.add("Carlos");
+        nomes.add("Jorge");
+
+        for (String nome : nomes){
+            System.out.println(nome);
+        }
+    }
+}
